8229a767050bf618f5306839b577690728353326
Ikpemhinoghena Etiuzale <etiuzaleikpemhinoghena@gmail.com>
2026-09-18 12:04:41 +0100

    Fix add to cart functionality

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,10 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from './utils.mjs';
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const storedCart = getLocalStorage('so-cart') || [];
+  const cartItems = Array.isArray(storedCart) ? storedCart : [storedCart];
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  document.querySelector('.product-list').innerHTML = htmlItems.join('');
 }
 
 function cartItemTemplate(item) {
--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,17 @@
-import { setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
+import ProductData from './ProductData.mjs';
 
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  if (!product) return;
+
+  const storedCart = getLocalStorage('so-cart') || [];
+  const cartItems = Array.isArray(storedCart) ? storedCart : [storedCart];
+  cartItems.push(product);
+  setLocalStorage('so-cart', cartItems);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
@@ -14,5 +20,5 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler);
